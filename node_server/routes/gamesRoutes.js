219416a903955d4e8e63ac48578c619d63471f09
Marcelo de Oliveira <marcelo_marchioro@hotmail.com>
2023-08-17 05:12:49 -0300

    Pronto para adicionar função de request do CHAT GPT e BACKEND feito também!

---
+++ node_server/routes/gamesRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const gamesApi = require('../controllers/gamesApi')
+const artworksApi = require('../controllers/artworksApi')
+
+router.post('/games', gamesApi.getGames)
+router.post('/artworks', artworksApi.getScreenshots)
+
+
+module.exports = router
