2f8fd6cc0b1110bf8220c1c1056428955f96ad0f
Marcelo de Oliveira <marcelo_marchioro@hotmail.com>
2023-08-31 11:11:51 -0300

    fix

--- node_server/services/checkVpn.js
+++ node_server/services/checkVpn.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.checkVpn = async (erro, req, res, next) => {
   const checkVpn = await axios.get(
-    `https://vpnapi.io/api/181.223.185.240?key=${process.env.VPN_DETECTION_API_KEY}`,
+    `https://vpnapi.io/api/${req.ip}?key=${process.env.VPN_DETECTION_API_KEY}`
   );
   if (checkVpn.data.security.vpn || checkVpn.data.security.proxy) {
     return true;
